a8a9c78df98036691dc37d818f840c65915e9793
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-05-27 07:29:25 -0700

    Fix direct response

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """View for State objects that handles default RESTful API actions"""
 
-from flask import jsonify, request, abort, make_response
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -31,21 +31,21 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'])
 def create_state():
     """Creates a State"""
     if not request.is_json:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
+        abort(400, description="Not a JSON")
     data = request.get_json()
     if 'name' not in data:
-        return make_response(jsonify({"error": "Missing name"}), 400)
+        abort(400, description="Missing name")
     new_state = State(**data)
     storage.new(new_state)
     storage.save()
-    return make_response(jsonify(new_state.to_dict()), 201)
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
@@ -55,11 +55,11 @@ def update_state(state_id):
     if not state:
         abort(404)
     if not request.is_json:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
+        abort(400, description="Not a JSON")
     data = request.get_json()
-    ignore_keys = ['id', 'created_at']
+    ignore_keys = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
         if key not in ignore_keys:
             setattr(state, key, value)
     storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    return jsonify(state.to_dict()), 200
